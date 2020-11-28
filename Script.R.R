acd98cc511ff9b1f51d577e8b836c459c456b707
Varathan, Pradeep <ppugale@iu.edu>
2020-11-28 11:48:41 -0500

    Update Script.R.R

--- Script.R.R
+++ Script.R.R
@@ -16,6 +16,7 @@ library(tidyverse)
 library(dplyr)
 library(PROPER)
 library(bench)
+library(pheatmap)
 
 # Preparing the GEO data for analysis - Real Data
 GSE40562 = getGEO("GSE40562")[[1]]
@@ -87,8 +88,7 @@ simulation_datasets = list('data1' = simRNAseq(simulation_options$sim1_opt,n1=4,
 
 # Running simulations on all three with benchmarking
 
-analysis_DESeq2 = bench(
-
+analysis_DESeq2 = mark(
     DESeq2_data1 = normalization_methods$DESeq2(simulation_datasets$data1$counts),
     DESeq2_data2 = normalization_methods$DESeq2(simulation_datasets$data2$counts),
     DESeq2_data3 = normalization_methods$DESeq2(simulation_datasets$data3$counts),
@@ -101,10 +101,9 @@ analysis_DESeq2 = bench(
 jpeg("DESeq2_Autoplot.jpeg")
 autoplot(analysis_DESeq2)
 dev.off()
-write.table(analysis_DESeq2,"DESeq2_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_DESeq2[,1:9],"DESeq2_Memory_Time_Analysis.txt",quote=F,row.names=F)
 
-analysis_PoissonSeq = bench(
-
+analysis_PoissonSeq = mark(
     PoissonSeq_data1 = normalization_methods$PoissonSeq(simulation_datasets$data1$counts,simulation_datasets$data1$designs),
     PoissonSeq_data2 = normalization_methods$PoissonSeq(simulation_datasets$data2$counts,simulation_datasets$data2$designs),
     PoissonSeq_data3 = normalization_methods$PoissonSeq(simulation_datasets$data3$counts,simulation_datasets$data3$designs),
@@ -117,10 +116,9 @@ analysis_PoissonSeq = bench(
 jpeg("PoissonSeq_Autoplot.jpeg")
 autoplot(analysis_PoissonSeq)
 dev.off()
-write.table(analysis_PoissonSeq,"PoissonSeq_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_PoissonSeq[,1:9],"PoissonSeq_Memory_Time_Analysis.txt",quote=F,row.names=F)
 
-analysis_TMM = bench(
-
+analysis_TMM = mark(
     TMM_data1 = normalization_methods$TMM(simulation_datasets$data1$counts),
     TMM_data2 = normalization_methods$TMM(simulation_datasets$data2$counts),
     TMM_data3 = normalization_methods$TMM(simulation_datasets$data3$counts),
@@ -133,11 +131,10 @@ analysis_TMM = bench(
 jpeg("TMM_Autoplot.jpeg")
 autoplot(analysis_TMM)
 dev.off()
-write.table(analysis_TMM,"TMM_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_TMM[,1:9],"TMM_Memory_Time_Analysis.txt",quote=F,row.names=F)
 
 
-analysis_RPKM = bench(
-
+analysis_RPKM = mark(
     RPKM_data1 = normalization_methods$RPKM(simulation_datasets$data1$counts),
     RPKM_data2 = normalization_methods$RPKM(simulation_datasets$data2$counts),
     RPKM_data3 = normalization_methods$RPKM(simulation_datasets$data3$counts),
@@ -150,59 +147,38 @@ analysis_RPKM = bench(
 jpeg("RPKM_Autoplot.jpeg")
 autoplot(analysis_RPKM)
 dev.off()
-write.table(analysis_RPKM,"RPKM_Memory_Time_Analysis.txt",quote=F,row.names=F)
-
-analysis_UQ = bench(
-    UQ_data1 = normalization_methods$UQ(simulation_datasets$data1$counts),
-    UQ_data2 = normalization_methods$UQ(simulation_datasets$data2$counts),
-    UQ_data3 = normalization_methods$UQ(simulation_datasets$data3$counts),
-    UQ_data4 = normalization_methods$UQ(simulation_datasets$data4$counts),
-    UQ_data5 = normalization_methods$UQ(simulation_datasets$data5$counts),
-    UQ_data6 = normalization_methods$UQ(simulation_datasets$data6$counts),
-    UQ_data7 = normalization_methods$UQ(simulation_datasets$data7$counts),
-    UQ_data8 = normalization_methods$UQ(simulation_datasets$data8$counts),
-    UQ_data9 = normalization_methods$UQ(simulation_datasets$data9$counts),check = FALSE)
-jpeg("UQ_Autoplot.jpeg")
-autoplot(analysis_UQ)
-dev.off()
-write.table(analysis_UQ,"UQ_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_RPKM[,1:9],"RPKM_Memory_Time_Analysis.txt",quote=F,row.names=F)
 
 # Running the real data
 
-analysis_GSE40562 = bench(
+analysis_GSE40562 = mark(
     DESeq2_R1 = normalization_methods$DESeq2(GSE40562_eset),
     PoissonSeq_R1 = normalization_methods$PoissonSeq(GSE40562_eset,get_subject_list(GSE40562$source_name_ch1,"normal")),
     TMM_R1 = normalization_methods$TMM(GSE40562_eset),
-    RPKM_R1 = normalization_methods$RPKM(GSE40562_eset),
-    UQ_R1 = normalization_methods$UQ(GSE40562_eset)
-)
+    RPKM_R1 = normalization_methods$RPKM(GSE40562_eset))
 jpeg("GSE40562.jpeg")
 autoplot(analysis_GSE40562)
 dev.off()
-write.table(analysis_GSE40562,"GSE40562_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_GSE40562[,1:9],"GSE40562_Memory_Time_Analysis.txt",quote=F,row.names=F)
 DESeq2_R1 = normalization_methods$DESeq2(GSE40562_eset)
 PoissonSeq_R1 = normalization_methods$PoissonSeq(GSE40562_eset,get_subject_list(GSE40562$source_name_ch1,"normal"))
 TMM_R1 = normalization_methods$TMM(GSE40562_eset)
 RPKM_R1 = normalization_methods$RPKM(GSE40562_eset)
-UQ_R1 = normalization_methods$UQ(GSE40562_eset)
 Raw_R1 = GSE40562_eset
 
-analysis_GSE98582 = bench(
+analysis_GSE98582 = mark(
     DESeq2_R2 = normalization_methods$DESeq2(GSE98582_eset),
     PoissonSeq_R2 = normalization_methods$PoissonSeq(GSE98582_eset,get_subject_list(GSE98582$characteristics_ch1.2,"Control")),
     TMM_R2 = normalization_methods$TMM(GSE98582_eset),
-    RPKM_R2 = normalization_methods$RPKM(GSE98582_eset),
-    UQ_R2 = normalization_methods$UQ(GSE98582_eset)
-)
+    RPKM_R2 = normalization_methods$RPKM(GSE98582_eset))
 jpeg("GSE98582.jpeg")
 autoplot(analysis_GSE98582)
 dev.off()
-write.table(analysis_GSE98582,"GSE98582_Memory_Time_Analysis.txt",quote=F,row.names=F)
+write.table(analysis_GSE98582[,1:9],"GSE98582_Memory_Time_Analysis.txt",quote=F,row.names=F)
 DESeq2_R2 = normalization_methods$DESeq2(GSE98582_eset)
 PoissonSeq_R2 = normalization_methods$PoissonSeq(GSE98582_eset,get_subject_list(GSE98582$characteristics_ch1.2,"Control"))
 TMM_R2 = normalization_methods$TMM(GSE98582_eset)
 RPKM_R2 = normalization_methods$RPKM(GSE98582_eset)
-UQ_R2 = normalization_methods$UQ(GSE98582_eset)
 Raw_R2 = GSE98582_eset
 
 # Comparing the post normalization count matrixes
@@ -213,7 +189,6 @@ boxplot(log2(DESeq2_R1))
 boxplot(log2(PoissonSeq_R1))
 boxplot(log2(TMM_R1))
 boxplot(log2(RPKM_R1))
-boxplot(log2(UQ_R1))
 boxplot(log2(Raw_R1))
 dev.off()
 
@@ -223,7 +198,6 @@ boxplot(log2(DESeq2_R2))
 boxplot(log2(PoissonSeq_R2))
 boxplot(log2(TMM_R2))
 boxplot(log2(RPKM_R2))
-boxplot(log2(UQ_R2))
 boxplot(log2(Raw_R2))
 dev.off()
 
@@ -234,7 +208,6 @@ pheatmap(log2(DESeq2_R1))
 pheatmap(log2(PoissonSeq_R1))
 pheatmap(log2(TMM_R1))
 pheatmap(log2(RPKM_R1))
-pheatmap(log2(UQ_R1))
 pheatmap(log2(Raw_R1))
 dev.off()
 
@@ -244,7 +217,6 @@ pheatmap(log2(DESeq2_R2))
 pheatmap(log2(PoissonSeq_R2))
 pheatmap(log2(TMM_R2))
 pheatmap(log2(RPKM_R2))
-pheatmap(log2(UQ_R2))
 pheatmap(log2(Raw_R2))
 dev.off()
 
